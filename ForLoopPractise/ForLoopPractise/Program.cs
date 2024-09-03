4ed9d965fe5921fccfd9b03f12afb27fb781da66
Oscar avendano <Oscaravendnaoo@hotmail.com>
2024-09-03 23:09:31 +0200

    made a folder for forloop practise

---
+++ ForLoopPractise/ForLoopPractise/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ForLoopPractise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // This program asks for two numbers and types all the numbers in between out to the user
+
+            Console.WriteLine("Hello, please type two number and i will type the numbers in between out for you.");
+            Console.WriteLine("first number;");
+            string firstNumberString = Console.ReadLine();
+            int firstNumberInt = int.Parse(firstNumberString);
+            Console.WriteLine("second number;");
+            string secondNumberString = Console.ReadLine();
+            int secondNumberInt = int.Parse(secondNumberString);
+            Console.WriteLine("");
+            Console.WriteLine("Here are the numbers;");
+
+            for (int i = firstNumberInt + 1; i < secondNumberInt; i++)
+            {
+                Console.Write(i + " ");
+
+
+            }
+            Console.WriteLine("");
+        }
+    }
+}
